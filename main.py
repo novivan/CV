456bcc69e7ee142cff453c1e2be504005bedb3e0
novivan <nov_ivan_777@mail.ru>
2024-07-11 16:31:58 +0300

    deleted extra things

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ path = 'cascades/haarcascade_frontalface_default.xml'
 face_detector = cv.CascadeClassifier(path)
 
 def detect():
-    '''rects = face_detector.detectMultiScale(gray_s,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv.CASCADE_SCALE_IMAGE)
-    for rect in rects: 
-        cv.rectangle(gray_s, rect, 255, 2)
-        '''
     rects = face_detector.detectMultiScale(pic_s,
         scaleFactor=1.1,
         minNeighbors=5,
@@ -30,18 +22,12 @@ size = (640, 360)
 while True:
 # Capture frame-by-frame 
     ret, frame = cap.read()
-
-    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #gray_s = cv.warpAffine(gray, M, size)
     pic = cv.cvtColor(frame, 0)
     pic_s = cv.warpAffine(pic, M, size)
 
     detect()
-
-    #cv.imshow('window', gray_s)
     cv.imshow('window', pic_s)
     t = time.time()
-    #cv.displayOverlay('window', f'time={t-t0:.3f}')
     t0 = t
     if cv.waitKey(1) & 0xFF == ord('q'): 
         break
